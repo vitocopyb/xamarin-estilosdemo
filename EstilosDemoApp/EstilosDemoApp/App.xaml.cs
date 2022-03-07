3814c0a68c91193723cc5ea215e4ec06ed660261
Victor Sepulveda <vsepulveda@gmail.com>
2022-03-07 01:41:01 -0300

    Primer commit

---
+++ EstilosDemoApp/EstilosDemoApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstilosDemoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new EstilosHeredadosPage();
+            MainPage = new TriggerStylePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
